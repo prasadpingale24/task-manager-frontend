07badc761ab5abd868c417461153b10865b7cbf1
prasadpingale24 <prasadpingale24@gmail.com>
2026-02-23 05:41:28 +0530

    fix: use env base url than localhost

--- src/api/client.ts
+++ src/api/client.ts
@@ -3,7 +3,7 @@ import { useAuthStore } from "../store/authStore";
 import toast from "react-hot-toast";
 
 export const apiClient = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
